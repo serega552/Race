73053d41151e8eb8f16bd19474108e15c690fc6b
serega552 <serezha-or@mail.ru>
2024-07-07 12:28:43 +0500

    LB

---
+++ Assets/Source/Initers/LanguageAuthorize.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using YG;
+
+public class LanguageAuthorize : MonoBehaviour
+{
+    public void Start()
+    {
+        if (YandexGame.EnvironmentData.language == "ru")
+        {
+            Lean.Localization.LeanLocalization.SetCurrentLanguageAll("Russian");
+        }
+        else if (YandexGame.EnvironmentData.language == "en")
+        {
+            Lean.Localization.LeanLocalization.SetCurrentLanguageAll("English");
+        }
+        else if (YandexGame.EnvironmentData.language == "tr")
+        {
+            Lean.Localization.LeanLocalization.SetCurrentLanguageAll("Turkish");
+        }
+        else
+        {
+            Lean.Localization.LeanLocalization.SetCurrentLanguageAll("English");
+        }
+    }
+}
